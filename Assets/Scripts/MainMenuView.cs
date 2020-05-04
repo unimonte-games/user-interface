deebce4fc7ee0589d45d96688cc47ff44c2c5bba
Danilo Bezerra <danilobezerra@outlook.com.br>
2020-05-04 13:59:51 -0300

    Programação de Interface de Usuário para Consoles - Parte 1

---
+++ Assets/Scripts/MainMenuView.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuView : View
+{
+    [SerializeField] private Text _titleText;
+    
+    public void Setup(string title)
+    {
+        Debug.Log("Teste");
+        _titleText.text = title;
+    }
+}
